eb5fe4b5beec39f50c8501cb450ae62c7c0da828
Nwogu <kachibunny@gmail.com>
2026-07-01 11:18:43 -0400

    updating resolution options for RNA best parameter code

--- Codes/RF_clustering_evaluation_analysis_RNA.py
+++ Codes/RF_clustering_evaluation_analysis_RNA.py
@@ -22,7 +22,7 @@ def save_dataframe(output_folder, df, file_name):
 def select_K_res(sparse_mat, embedding,jobs =1, SNN_prune=None):
     print('starting KNN algorithm')
     output_df = pd.DataFrame(columns=['K', 'resolution', 'Number_of_clusters','CH_score','Silhouette'])
-    K_list = [20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120]
+    K_list = [230, 240, 250, 260, 270, 280, 290, 300, 310, 320]
     
     for k in K_list:
         print(f'Working on k {k}')
@@ -132,12 +132,14 @@ def main():
     parser = argparse.ArgumentParser(description='COMMOT pipeline',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('reduced_obj', metavar='file', help='csv file that contains the pca reduced cell embeddings')
     parser.add_argument('jobs', type=int, help='The number of jobs or workers to run for the KNN and ensemble tree algorithms')
+    #parser.add_argument('min_sample_split',type =int, help = 'The minimum number of samples required to split an internal node', default=2)
     parser.add_argument("--output_dir", type=str, default="./data",help="Path to the output folder (default: ./data)")
     parser.add_argument("file_name_clusters", type=str, default="./data/cluster_evaluation.csv",help="Name of output file (default: ./data/cluster_evaluation.csv)")
 
     args = parser.parse_args()
 
     pca_emb = pd.read_csv(args.reduced_obj, sep = ',', index_col=0)
+    print(pca_emb.shape)
     
     print('starting ensemble tree model')
     start_computation = perf_counter()
